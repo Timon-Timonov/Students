1885067d20c2368ef30f16fbe0c572dd7b6a5eb4
Timon <Timon321.86@gmail.com>
2024-01-12 18:02:45 +0300

    Any changes after remarks.

--- src/main/java/it_academy/Client.java
+++ src/main/java/it_academy/Client.java
@@ -3,8 +3,10 @@ package it_academy;
 import it_academy.context.Student;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class Client {
 
@@ -15,34 +17,28 @@ public class Client {
 
 	public static void main(String[] args) {
 
-		group = createGroupOfStudents(ConstantContainer.COUNT_OF_TYPES_OF_STUDENTS,
-				ConstantContainer.COUNT_OF_STUDENTS_OF_EACH_TYPE);
+		group = createGroupOfStudents(ConstantContainer.COUNT_OF_STUDENTS_OF_EACH_TYPE);
 
 		printGroup(group, OK_MESSAGE);
-/*
-		//experiment with chang of type:
-		group.get(group.size() - 1).setType(ConstantContainer.Types.values()[0]);
-		group.get(group.size() - 2).setType(ConstantContainer.Types.values()[0]);
-		printGroup(group, CHANGE_MESSAGE);
-		*/
+/*		//experiment with chang of type:
+		group.get(group.size() - 1).setType(Types.values()[0]);
+		group.get(group.size() - 2).setType(Types.values()[0]);
+		printGroup(group, CHANGE_MESSAGE);*/
 	}
 
-	public static List<Student> createGroupOfStudents(int types, int countOfSTudentEachType) {
+	public static List<Student> createGroupOfStudents(int countOfSTudentEachType) {
 		List<Student> list = new ArrayList<>();
-		for (int i = 0; i < types; i++) {
-
-			for (int j = 0; j < countOfSTudentEachType; j++) {
-				list.add(new Student(ConstantContainer.Types.values()[i], getRanTalent(random)));
-			}
+		for (int i = 0; i < countOfSTudentEachType; i++) {
+			list.addAll(Arrays.stream(Types.values())
+					.map(typ -> new Student(typ, getRanTalent(random)))
+					.collect(Collectors.toList()));
 		}
 		return list;
 	}
 
 	private static void printGroup(List<Student> group, String message) {
 		System.out.println(message);
-		for (Student st : group) {
-			System.out.println(st.toString());
-		}
+		group.forEach(System.out::println);
 		System.out.println();
 	}
 
